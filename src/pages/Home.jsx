d5dbf730f5cce979c510cf49e9f559885ffebb28
chaim <chaim.triebitz@bhbsdventures.com>
2024-09-19 16:57:56 +0300

    mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,15 +20,16 @@ export const Home = () => {
    return (
       <div className='page home'>
          <div className="banner home">
+
+            <h1>Experienced Real Estate Lawyer in</h1>
+            <span className='isreal'>Israel</span>
             <div className='card2'>
                <img src={imgs.office} alt="img" />
             </div>
-
-            <h1>Experienced Real Estate Lawyer for Property Transactions in Israel</h1>
             <h2>Netanel Fraiman</h2>
             <div className='rotating-text-container' style={{ display: "block" }}>
                <RotatingText
-                  staticText={'Providing expert legal advice in'}
+                  staticText={'Providing expert legal advice in :'}
                   rotatingTexts={["Property Deals", "Tax Planning", "Financial Plans"]}
                />
             </div>
